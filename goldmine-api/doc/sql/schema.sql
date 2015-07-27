65b0c538de2f325b998a7ab458a89b108513b729
黄勇 <yong.huang@adchina.com>
2015-07-27 17:24:36 +0800

    添加 last_modified 列

--- goldmine-api/doc/sql/schema.sql
+++ goldmine-api/doc/sql/schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE `advertiser` (
   `id` char(32) NOT NULL,
   `advertiser_name` varchar(64) NOT NULL DEFAULT '' COMMENT '名称',
   `description` varchar(1024) DEFAULT '' COMMENT '描述',
-  `created_time` char(19) DEFAULT NULL,
+  `created_time` char(19) DEFAULT NULL COMMENT '创建时间',
+  `last_modified` datetime DEFAULT NULL COMMENT '最后修改时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

e718703470832ead5508593e8a8ebae7ae51cff8
黄勇 <yong.huang@adchina.com>
2015-08-17 13:52:00 +0800

    支持拦截器方式进行 token 验证

--- api-framework/src/main/java/com/adchina/api/security/SecurityInterceptor.java
+++ api-framework/src/main/java/com/adchina/api/security/SecurityInterceptor.java
@@ -1,18 +1,19 @@
 package com.adchina.api.security;
 
 import com.adchina.api.util.StringUtil;
-import com.adchina.api.web.WebContext;
 import java.lang.reflect.Method;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.reflect.MethodSignature;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
- * 用于检查 token 的切面
+ * 用于检查 token 的拦截器
  *
  * @author huangyong
  * @since 1.0.0
  */
-public class SecurityAspect {
+public class SecurityInterceptor extends HandlerInterceptorAdapter {
 
     private static final String DEFAULT_TOKEN_NAME = "X-Token";
 
@@ -30,22 +31,23 @@ public class SecurityAspect {
         this.tokenName = tokenName;
     }
 
-    public Object execute(ProceedingJoinPoint pjp) throws Throwable {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 从切点上获取目标方法
-        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
-        Method method = methodSignature.getMethod();
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
         // 若目标方法忽略了安全性检查，则直接调用目标方法
         if (method.isAnnotationPresent(IgnoreSecurity.class)) {
-            return pjp.proceed();
+            return true;
         }
         // 从 request header 中获取当前 token
-        String token = WebContext.getRequest().getHeader(tokenName);
+        String token = request.getHeader(tokenName);
         // 检查 token 有效性
         if (!tokenManager.checkToken(token)) {
             String message = String.format("token [%s] is invalid", token);
             throw new TokenException(message);
         }
         // 调用目标方法
-        return pjp.proceed();
+        return true;
     }
 }
